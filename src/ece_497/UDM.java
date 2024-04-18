0e6238b48dac90b858dc3dae08e56f4b3cbdd233
kmonahan2 <123402763+kmonahan2@users.noreply.github.com>
2024-04-18 09:40:28 -0500

    ??

--- src/ece_497/UDM.java
+++ src/ece_497/UDM.java
@@ -22,7 +22,7 @@ public class UDM {
 
     public UDM() {
         // starting variables
-        this.known_ue = Main.CSVToJTable("known_ue.csv");
+        this.known_ue = CSVToJTable("KnownUE.csv");
         this.entered_ue = new ArrayList<UE>();
         this.registered_ue = new JTable();
         this.general_width = 500;
@@ -148,6 +148,43 @@ public class UDM {
             data_bd.setValueAt(temp, i, 5);
         }
 
+    }
+
+    // helper function
+    public static JTable CSVToJTable(String filename) {
+        JTable table = new JTable();
+        table.setName(filename);
+        
+        // Create a table model
+        DefaultTableModel model = new DefaultTableModel();
+        // Headers: UE,Priority,Callbar,Prempt_Capable,Prempt_Vul,AppType
+        model.addColumn("UE");
+        model.addColumn("Priority");
+        model.addColumn("Callbar");
+        model.addColumn("Prempt_Capable");
+        model.addColumn("Prempt_Vul");
+        model.addColumn("AppType");
+        
+
+        // Read CSV file and add rows to the table
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(","); // Assuming CSV uses comma as delimiter, change accordingly
+                model.addRow(data);
+            }
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // build the jtable
+        table = new JTable(model);
+        JFrame frame = new JFrame();
+        frame.add(new JScrollPane(table));
+
+        return table;
     }
 
     // helper function
